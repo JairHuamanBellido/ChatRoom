31e01787e48df250b57a22090e7352f25df4cdc9
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2019-04-23 18:02:04 -0500

    se añadio el html

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const express = require('express');
 const app =  require('express')();
 const PORT = process.env.PORT || 3000;
 const router =  require('./src/Router/router');
+const path = require('path')
 
 
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'views')));
 
+
+app.set('view engine','ejs');
 app.use('/',router.router);
 
 app.listen(PORT);
--- src/Router/router.js
+++ src/Router/router.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router =  express.Router();
 
-router.get('/login', async(req,res)=>{
-    res.send('Esta en el login')
+router.get('/', async(req,res)=>{
+    res.redirect('/login');
 })
+
+router.get('/login', async(req,res)=>{
+    res.render('login');
+})
+
 
 router.get('/chat', async(req,res)=>{
     res.send('Esta en el chat');
 })
 
+
+router.post('/chat', async(req,res)=>{
+    res.redirect('/chat');
+})
 
 module.exports.router =  router;
