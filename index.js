31641d15c7c57c790dc5fa4baf4d216ad19223d7
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2019-04-23 19:22:17 -0500

    Se añadieron los sockets

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const app =  require('express')();
+const app = express();
 const session = require('express-session')
-const http =  require('http').Server(app);
+const server = require('http').Server(app);
 const PORT = process.env.PORT || 3000;
-const router =  require('./src/Router/router');
+const router = require('./src/Router/router');
 const path = require('path')
-const io =  require('socket.io')(http);
+const io = require('socket.io')(server);
 
 
 app.use(session({
@@ -13,16 +13,22 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false }
-  }))
-app.use(express.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname,'views')));
+}))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.use('/public', express.static('public'))
-app.set('view engine','ejs');
-app.use('/',router.router);
+app.set('view engine', 'ejs');
 
-io.on('connection', (socket)=>{
-    console.log('user and enconnect');
+
+app.use('/', router.router);
+
+io.on('connection', (socket) => {
+    socket.on('send message', (data)=>{
+        io.sockets.emit('display message',{message:data.message,username:data.username})
+        
+    })
+
 })
 
-http.listen(PORT);
+server.listen(PORT);
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,20 @@
-const socket =  io();
+let socket = io();
+const messageInput = document.getElementById('field-message');
+const containerMessage = document.getElementById('container-message');
+const username = document.getElementsByClassName("header-user")[0].getAttribute("id");
+const sendMessage = () => {
+    if (messageInput.value != "") {
+        socket.emit('send message', { message: messageInput.value, username: username });
+    }
+}
+
+socket.on('display message', (data)=>{
+    console.log("Username: "+ data.username);
+    console.log("Mensaje : "+ data.message );
+    let messageContainer =  document.createElement('div');
+    messageContainer.innerHTML = `
+        <p>${data.username}: ${data.message}</p>
+    `
+
+    containerMessage.appendChild(messageContainer);
+})
